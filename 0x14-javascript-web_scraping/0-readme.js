8fbcfbaa6ebcf3de0edddd19188f2fa1cb7ffe67
Ajoe62 <ijeborjoe@gmail.com>
2024-02-20 03:16:35 -0800

    update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,11 +4,16 @@
 // Import the 'fs' module for file system operations
 const fs = require('fs');
 
+// Get the filename from the command-line arguments
 const filename = process.argv[2];
 
+// Use 'fs.readFile' to read the file asynchronously
 fs.readFile(filename, 'utf-8', (error, content) => {
+
+  // If an error occurred, log it to the console
   if (error) {
     console.log(error);
+ // Otherwise, print the content of the file
   } else {
     console.log(content);
   }
